bc2ae57ee608d8181c36d46fa194a5c294dbe2bd
Peter <Peterlee2094@gmail.com>
2024-01-31 14:36:18 -0600

    create uesr pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import YearPage from "./pages/year/YearPage.tsx";
 import MonthPage from "./pages/month/MonthPage.tsx";
-
+// import
 import "./index.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SignUpPage from "./pages/user/SignUpPage.tsx";
+import LoginPage from "./pages/user/LoginPage.tsx";
+import UserPage from "./pages/user/UserPage.tsx";
 
 const router = createBrowserRouter([
   {
@@ -19,6 +22,18 @@ const router = createBrowserRouter([
   {
     path: "/month/:year/:month/:mode",
     element: <MonthPage />,
+  },
+  {
+    path: "/user",
+    element: <UserPage />,
+  },
+  {
+    path: "/sign-up",
+    element: <SignUpPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
   },
 ]);
 
