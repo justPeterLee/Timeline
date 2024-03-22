9d86fa5bbb843a912889667e05857f5f828162fd
Peter <Peterlee2094@gmail.com>
2024-03-22 08:34:46 -0500

    create TimelineCardAnimation

--- client/src/components/Timelines/Timeline/Timeline.tsx
+++ client/src/components/Timelines/Timeline/Timeline.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import styles from "./Timeline.module.css";
+import { SpringValues, animated, to } from "react-spring";
 
 export function TimelineCard({
   children,
@@ -14,5 +15,39 @@ export function TimelineCard({
     <div className={styles.TimelineCard} id={id} style={{ ...style }}>
       {children}
     </div>
+  );
+}
+
+type TimelineSpringVlaue = SpringValues<{
+  x: number;
+  scale: number;
+  origin: number;
+  markerX: number;
+  markerDayOpacity: number;
+}>;
+
+export function TimelineCardAnimation({
+  timelineSpring,
+  id,
+  children,
+}: {
+  timelineSpring: TimelineSpringVlaue;
+  id: string;
+  children: ReactNode;
+}) {
+  return (
+    <animated.div
+      className={styles.Timeline}
+      id={id}
+      style={{
+        transformOrigin: timelineSpring.origin.to((value) => `${value}%`),
+        transform: to(
+          [timelineSpring.scale, timelineSpring.x],
+          (scale, x) => `scaleX(${scale}) translate(${x}px,-50%)`
+        ),
+      }}
+    >
+      {children}
+    </animated.div>
   );
 }
