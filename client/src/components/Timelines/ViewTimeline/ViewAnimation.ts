d6e02706656d4bfb5841f36cf2424558c1d1c4d1
Peter <Peterlee2094@gmail.com>
2024-03-22 08:52:40 -0500

    create ViewAnimation

---
+++ client/src/components/Timelines/ViewTimeline/ViewAnimation.ts
@@ -0,0 +1,48 @@
+import { monthByIndex } from "../../../tools/data/monthData";
+
+export function calcOriginPercent(
+  selectedMonth: string,
+  scaleRatio: number,
+  timelineContainer: HTMLDivElement
+) {
+  const index = parseInt(selectedMonth);
+
+  if (index <= 1) {
+    return 0;
+  }
+  if (index >= 12) {
+    return 100;
+  }
+
+  const timelineContainerBCR = timelineContainer.getBoundingClientRect();
+
+  const markerPos =
+    (1 / 365) *
+      (monthByIndex[index].dayOfYear - 1) *
+      (timelineContainerBCR.width * scaleRatio) +
+    timelineContainerBCR.x;
+
+  const gapIdeal = 0.05 * timelineContainerBCR.width + timelineContainerBCR.x;
+
+  const transGap = markerPos - gapIdeal;
+
+  const scaleRatioPerPercent =
+    (scaleRatio - 1) * (0.01 * timelineContainerBCR.width);
+
+  const originPercent = transGap / scaleRatioPerPercent;
+
+  return originPercent;
+}
+
+export function findScaleRatio(
+  days: number,
+  timelineContainer: HTMLDivElement
+) {
+  const timelineContainerBCR = timelineContainer.getBoundingClientRect();
+  const gap = (1 / 365) * days * timelineContainerBCR.width;
+  const gapIdeal = timelineContainerBCR.width * 0.9;
+
+  const scaleRatio = gapIdeal / gap;
+
+  return scaleRatio;
+}
