c05952c172e349d5283cb8dd8fd8b49136a9542a
Peter <Peterlee2094@gmail.com>
2024-04-06 10:08:23 -0500

    create ui for create new year modal

--- client/src/components/UserComponents/PoleSection.tsx
+++ client/src/components/UserComponents/PoleSection.tsx
@@ -1,8 +1,10 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { AllStandardPoleData } from "../../tools/utilities/timepoleUtils/timepoleUtils";
 import styles from "./PoleSection.module.css";
 import { format } from "date-fns";
 import { useNavigate } from "react-router-dom";
+import { Modal } from "../elements/Links";
+import { current } from "../../tools/data/monthData";
 // import {format}
 export function PoleSectionContainer({
   poles,
@@ -11,17 +13,43 @@ export function PoleSectionContainer({
     [key: string]: { [key: string]: AllStandardPoleData[] };
   };
 }) {
+  const [createModal, setCreateModal] = useState(false);
+
   const polesKeys = useMemo(() => {
     return Object.keys(poles);
   }, [poles]);
   return (
-    <div className={styles.PoleSectionContainer}>
-      {/* <p>{}</p> */}
-      {polesKeys.map((_pole) => {
-        const poleData = poles[_pole];
-        return <PoleSection key={_pole} poleDataObj={poleData} year={_pole} />;
-      })}
-    </div>
+    <>
+      <div className={styles.PoleSectionContainer}>
+        {/* <p>{}</p> */}
+        {polesKeys.map((_pole) => {
+          const poleData = poles[_pole];
+          return (
+            <PoleSection key={_pole} poleDataObj={poleData} year={_pole} />
+          );
+        })}
+        <div
+          className={styles.CreateNewYear}
+          onClick={() => {
+            setCreateModal(true);
+          }}
+        ></div>
+      </div>
+
+      {createModal && (
+        <Modal
+          onClose={() => {
+            setCreateModal(false);
+          }}
+        >
+          <CreateNewYearModal
+            onClose={() => {
+              setCreateModal(false);
+            }}
+          />
+        </Modal>
+      )}
+    </>
   );
 }
 
@@ -91,6 +119,89 @@ function Section({ poleData }: { poleData: AllStandardPoleData }) {
         {format(new Date(poleData.full_date), "MMM io")}
       </p>
       <p>{poleData.title}</p>
+    </div>
+  );
+}
+
+import { IoMdArrowDropdown } from "react-icons/io";
+import { FaCheck } from "react-icons/fa";
+
+function CreateNewYearModal({ onClose }: { onClose: () => void }) {
+  const [selectedYear, setSelectedYear] = useState(current.year);
+  const [focus, setFocus] = useState(false);
+  return (
+    <div className={styles.CreateNewYearModal}>
+      <div className={styles.CNYMTitle}>
+        <p>Create New Timeline</p>
+      </div>
+
+      <div className={styles.CNYMInput}>
+        <button
+          className={styles.CNYMDatalist}
+          onClick={() => {
+            setFocus(!focus);
+          }}
+        >
+          <div
+            className={styles.CNYMYear}
+            style={focus ? { border: "solid 1px rgb(200,200,200)" } : {}}
+          >
+            <p>{selectedYear}</p>
+            <IoMdArrowDropdown className={styles.CNYMDropIcon} />
+          </div>
+        </button>
+
+        {focus && (
+          <div className={styles.CNYMYearDatalistValues}>
+            <NewYearDataList
+              value={selectedYear}
+              setValue={(newYear) => {
+                setSelectedYear(newYear);
+                setFocus(false);
+                //   onClose();
+              }}
+            />
+          </div>
+        )}
+      </div>
+
+      <div className={styles.CNYMButton}>
+        <button className={styles.CNYMCancel} onClick={onClose}>
+          cancel
+        </button>
+        <button className={styles.CNYMCreate}>create</button>
+      </div>
+    </div>
+  );
+}
+
+function NewYearDataList({
+  value,
+  setValue,
+}: {
+  value: number;
+  setValue: (newYear: number) => void;
+}) {
+  return (
+    <div className={styles.DataListContainer}>
+      {Array.from({ length: 11 }, (_, index) => {
+        const position = index;
+        const listValue = value - 5 + position;
+        return (
+          <div
+            key={Math.random()}
+            className={styles.ListValue}
+            onClick={() => {
+              setValue(listValue);
+            }}
+          >
+            {listValue}
+            {listValue === value && (
+              <FaCheck className={styles.CheckIcon} size={10} />
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 }
