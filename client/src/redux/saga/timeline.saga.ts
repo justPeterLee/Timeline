4d9efe0d49b7aa4a7540251cc4a02791fb84fca3
Peter <Peterlee2094@gmail.com>
2024-04-08 07:04:33 -0500

    create post route for timeline in sagas

--- client/src/redux/saga/timeline.saga.ts
+++ client/src/redux/saga/timeline.saga.ts
@@ -3,16 +3,32 @@ import axios from "axios";
 
 function* timelineSaga() {
   yield takeLatest("GET_TIMELINE_SERVER", getTimelineUser);
+  yield takeLatest("POST_TIMELINE_SERVER", postTimelineUser);
 }
 
-function* getTimelineUser() {
+function* getTimelineUser(): Generator {
   try {
     console.log("in saga");
-    yield axios.get("/api/v1/timeline/get");
-    yield put({ type: "SET_USER_TIMELINE", payload: [] });
+    const data: any = yield axios.get("/api/v1/timeline/get");
+    yield put({ type: "SET_USER_TIMELINE", payload: data });
   } catch (err) {
     console.log(err);
     throw "error with fetching user timeline";
+  }
+}
+
+function* postTimelineUser({
+  payload,
+}: {
+  payload: { title: string; year: string };
+  type: string;
+}) {
+  try {
+    yield axios.post("/api/v1/timeline/create", payload);
+    yield put({ type: "GET_TIMELINE_SERVER" });
+  } catch (err) {
+    console.log(err);
+    throw "error with create timeline";
   }
 }
 export default timelineSaga;
